22500818da5c0288ba7b4c54f08204ce98f1f264
Robinson Pires <pires@IAZI.LOCAL>
2017-07-19 09:49:15 +0530

    code

---
+++ src/pages/home/home.ts
@@ -0,0 +1,70 @@
+import {Component} from '@angular/core';
+import {NavController, AlertController} from 'ionic-angular';
+import {Camera, CameraOptions} from '@ionic-native/camera';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public photos : any;
+  public base64Image : string;
+  constructor(public navCtrl : NavController, private camera : Camera, private alertCtrl : AlertController) {}
+
+  ngOnInit() {
+    this.photos = [];
+  }
+
+  deletePhoto(index) {
+    let confirm = this
+      .alertCtrl
+      .create({
+        title: 'Sure you want to delete this photo? There is NO undo!',
+        message: '',
+        buttons: [
+          {
+            text: 'No',
+            handler: () => {
+              console.log('Disagree clicked');
+            }
+          }, {
+            text: 'Yes',
+            handler: () => {
+              console.log('Agree clicked');
+              this
+                .photos
+                .splice(index, 1);
+              //return true;
+            }
+          }
+        ]
+      });
+    confirm.present();
+  }
+
+  takePhoto() {
+    const options : CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this
+      .camera
+      .getPicture(options)
+      .then((imageData) => {
+        // this.base64Image = "file://" + imageData;
+        this.base64Image = 'data:image/jpeg;base64,'+ imageData;
+        this
+          .photos
+          .push(this.base64Image);
+        this
+          .photos
+          .reverse();
+      }, (err) => {
+        console.log(err);
+      });
+  }
+
+}
