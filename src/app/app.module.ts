1820eceb3423874456b22e33749579e997165117
Robinson Pires <pires@IAZI.LOCAL>
2017-07-20 09:20:47 +0530

    code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { PropertyCardList } from '../pages/card/property-card-list';
+import { PropertyCard } from '../pages/card/property-card';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {Camera} from '@ionic-native/camera';
 import { Geolocation } from '@ionic-native/geolocation';
 
+import { GeoLocationService } from './geo-location.service';
+import { CameraService } from './camera.service';
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    PropertyCardList,
+    PropertyCard
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    PropertyCardList,
+    PropertyCard
   ],
   providers: [
     StatusBar,
     SplashScreen,
     Camera,
     Geolocation,
+    GeoLocationService,
+    CameraService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController, AlertController} from 'ionic-angular';
-import {Camera, CameraOptions} from '@ionic-native/camera';
-import { Geolocation } from '@ionic-native/geolocation';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,90 +7,11 @@ import { Geolocation } from '@ionic-native/geolocation';
 })
 export class HomePage {
 
-  public photos : IGallery[];
-  public base64Image : string;
-  constructor(public navCtrl : NavController, 
-              private camera : Camera, 
-              private alertCtrl : AlertController,
-              private geolocation: Geolocation
+  constructor(public navCtrl : NavController
             ) {}
 
   ngOnInit() {
-    this.photos = [];
-
   }
-
-  deletePhoto(index) {
-    let confirm = this
-      .alertCtrl
-      .create({
-        title: 'Sure you want to delete this photo? There is NO undo!',
-        message: '',
-        buttons: [
-          {
-            text: 'No',
-            handler: () => {
-              console.log('Disagree clicked');
-            }
-          }, {
-            text: 'Yes',
-            handler: () => {
-              console.log('Agree clicked');
-              this
-                .photos
-                .splice(index, 1);
-              //return true;
-            }
-          }
-        ]
-      });
-    confirm.present();
-  }
-
-  takePhoto() {
-    const options : CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
-    this
-      .camera
-      .getPicture(options)
-      .then((imageData) => {
-        // this.base64Image = "file://" + imageData;
-        this.base64Image = 'data:image/jpeg;base64,'+ imageData;
-
-        let lat = 0;
-        let lon = 0;
-        //Get cordinates
-
-        this.geolocation.getCurrentPosition()
-            .then((resp) => {
-              lat = resp.coords.latitude;
-              lon = resp.coords.longitude;
-              console.log(lat + ' ' + lon);
-        }).catch((error) => {
-          console.log('Error getting location', error);
-        }).then(function(){
-            console.log(this.glob);
-            this
-              .photos
-              .push( {imageData: this.base64Image, latitude: lat, longitude: lon});
-            this
-              .photos
-              .reverse();
-        }.bind(this));
-      }, (err) => {
-        console.log(err);
-      });
-  }
-
 }
 
 
-export interface IGallery {
-    imageData: string;
-    latitude: number;
-    longitude: number;
-}
